30baead061fe2bad54d6f8057058289efb3b951f
chaitanyaksingh2005 <chaitanyaksingh2005@gmail.com>
2026-09-02 11:12:17 +0530

    Fix Flake8 formatting

--- cart.py
+++ cart.py
@@ -4,6 +4,7 @@ def cart_total(items):
         total = total + item["price"] * item["qty"]
     return total
 
+
 def apply_discount(total, percent):
     if percent < 0 or percent > 100:
         raise ValueError("percent must be between 0 and 100")
